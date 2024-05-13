f9fd192171b64ddadd8b7c7e4c37f7202ef29acf
IcyCreme <giulianoyv@gmail.com>
2024-05-12 20:52:19 -0400

    finish

--- webSearch.py
+++ webSearch.py
@@ -1,1 +1,10 @@
-print();
+#take each line from searches.txt and search the respective text
+from googlesearch import search
+
+with open('searches.txt', 'r') as searches:
+    for query in searches:
+        print(query + ": ")
+        for result in search(query, tld="co.in", num=10, stop=10, pause=1):
+            print(result)
+        print("--------------------------------------")
+        
